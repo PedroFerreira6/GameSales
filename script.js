0f57127d026a681a4063ebbc4c11fa0b07eb89a7
Otaniel Junior <otaniel1412@gmail.com>
2024-04-30 12:39:52 +0100

    update script

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+var dataSteam = [];
 // Make a GET request to your Node.js server endpoint
 $.get('http://localhost:3000/jogosemsaldo', function(data) {
   // Handle the response data (list of games on sale)
   // You can display this data on your website however you like
-  console.log(data);
+        
+  dataFuncPush(data);
+    
+    
+  //console.log(data);
 });
+function dataFuncPush(dataGlobal){
+    dataSteam.push(dataGlobal)
+    
+}
+console.log(dataSteam);
+
+

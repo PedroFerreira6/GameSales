69fd8a8c1fce5e8faecc40ec0674942f0946c473
Otaniel Junior <otaniel1412@gmail.com>
2024-04-30 10:58:22 +0100

    initial creation

--- script.js
+++ script.js
@@ -1,67 +1,24 @@
-// Function to search for an artist
-async function searchArtist() {
-    const artistName = document.getElementById("artist-input").value;
-    const tracksContainer = document.getElementById("tracks-container");
-    tracksContainer.innerHTML = ""; // Clear previous search results
+console.log('Init App!');
 
-    try {
-        // Search for the artist on Spotify
-        const spotifyTracks = await searchSpotify(artistName);
-        displayTracks(spotifyTracks, "Spotify");
+urlSteam()
 
-        // Search for the artist on YouTube
-        const youtubeTracks = await searchYouTube(artistName);
-        displayTracks(youtubeTracks, "YouTube");
-
-        // Search for the artist on SoundCloud
-        const soundCloudTracks = await searchSoundCloud(artistName);
-        displayTracks(soundCloudTracks, "SoundCloud");
-    } catch (error) {
-        console.error("Error searching for artist:", error);
-    }
+function pedido_GetSTEAM(url, callback){
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.addEventListener('load', callback);
+    xhr.send();
 }
 
-// Function to search for artist on Spotify
-async function searchSpotify(artistName) {
-    // Make API call to Spotify
-    // For example:
-    const response = await fetch(`https://api.spotify.com/v1/search?q=${artistName}&type=track`);
-    const data = await response.json();
-    return data.tracks.items;
+function urlSteam(){
+    let urlSteamSales = 'https://store.steampowered.com/api/featuredcategories/?l=portuguese' 
+    console.log(" teste url "+urlSteamSales);
+
+    pedido_GetSTEAM(urlSteamSales, showGames)
+
 }
 
-// Function to search for artist on YouTube
-async function searchYouTube(artistName) {
-    // Make API call to YouTube
-    // For example:
-    const response = await fetch(`https://www.googleapis.com/youtube/v3/search?q=${artistName}&key=YOUR_YOUTUBE_API_KEY`);
-    const data = await response.json();
-    return data.items;
+function showGames(){
+    console.log(this.responseText);
+    let games = JSON.parse(this.responseText)   
 }
 
-// Function to search for artist on SoundCloud
-async function searchSoundCloud(artistName) {
-    // Make API call to SoundCloud
-    // For example:
-    const response = await fetch(`https://api.soundcloud.com/tracks?q=${artistName}&client_id=YOUR_SOUNDCLOUD_CLIENT_ID`);
-    const data = await response.json();
-    return data;
-}
-
-// Function to display tracks
-function displayTracks(tracks, source) {
-    const tracksContainer = document.getElementById("tracks-container");
-    tracks.forEach(track => {
-        const trackElement = document.createElement("div");
-        trackElement.classList.add("track");
-        trackElement.innerText = track.name + " (" + source + ")";
-        trackElement.addEventListener("click", () => playTrack(track, source));
-        tracksContainer.appendChild(trackElement);
-    });
-}
-
-// Function to play a track
-function playTrack(track, source) {
-    // Implement playing track based on the source (Spotify, YouTube, SoundCloud)
-    console.log("Playing", track.name, "from", source);
-}

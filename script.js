c584a452c9be5d242bd747b23249faab8c100b1e
Otaniel Junior <otaniel1412@gmail.com>
2024-05-07 10:53:24 +0100

    update script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var dataSteam = [];
 // Make a GET request to your Node.js server endpoint
-$.get('http://localhost:3000/jogosemsaldo', function(data) {
+$.get('https://gamesales-production.up.railway.app/jogosemsaldo', function(data) {
   // Handle the response data (list of games on sale)
   // You can display this data on your website however you like
         

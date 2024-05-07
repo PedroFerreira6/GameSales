eb226c5cd80df0c0f57f9d8298e3f4a9081baa0a
CAP <rematocapitao321@gmail.com>
2024-05-07 12:44:13 +0100

    Update product_details.js

--- product_details.js
+++ product_details.js
@@ -1,26 +1,38 @@
-//função recolhe o id através do URL(GET)
+var xhr = new XMLHttpRequest();
+
+// funçao para buscar o id do jogo no url
 function getQueryParam(name) {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(name);
 }
-
+// id do url
 var gameId = getQueryParam('id');
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://gamesales-production.up.railway.app/jogosemsaldo', true);
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-            var data = JSON.parse(xhr.responseText);
-            var topSellers = data.top_sellers.items.slice(0, 9); // Seleciona apenas alguns dos elementos dos topsellers
+if (!gameId) {
+    console.error('Game ID não foi encontrado no URL');
+} else {
+    // 
+    xhr.open('GET', 'https://gamesales-production.up.railway.app/getGameDetails?id='+gameId, true);
 
-            // Filtra os itens duplicados com base nos seus IDs
-            var uniqueTopSellers = topSellers.filter((item, index, self) =>
-                index === self.findIndex((t) => (
-                    t.id === item.id
-                ))
-            );
+    // 
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText);
+                // verifica se 
+                if (data && data[gameId] && data[gameId].success) {
+                    // Retrieve the detailed description of the game
+                    var detailedDescription = data[gameId].data.detailed_description;
+                    console.log(detailedDescription);
+                } else {
+                    console.error('Game details not found for ID:', gameId);
+                }
+            } else {
+                console.error('Error fetching game details:', xhr.status);
+            }
         }
-    }
+    };
 
+    // Send the request
+    xhr.send();
 }

f35b65dcc96f9bb0289f51006d41509ca5c52b53
Otaniel Junior <otaniel1412@gmail.com>
2024-05-09 11:34:44 +0100

    update

--- front_page_game.js
+++ front_page_game.js
@@ -1,21 +1,48 @@
-var xhr = new XMLHttpRequest();
+var xhr3 = new XMLHttpRequest();
 
-xhr.open('GET', 'https://gamesales-production.up.railway.app/jogosemsaldo', true);
+xhr3.open('GET', 'https://gamesales-production.up.railway.app/jogosemsaldo', true);
 
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE){
-        if (xhr.status === 200){
-            var data = JSON.parse(xhr.responseText);
-            var topSellers = data.top_sellers.items(0, 1);
+xhr3.onreadystatechange = function () {
+    
+    if (xhr3.readyState === XMLHttpRequest.DONE) {
+        console.log("chegou1");
+        if (xhr3.status === 200){
+            var data = JSON.parse(xhr3.responseText);
+            var topSellers = data.top_sellers.items.slice(0, 9);
 
             var uniqueTopSellers = topSellers.filter((item, index, self) =>
                 index === self.findIndex((t) => (
                     t.id === item.id
                 ))
             );
+            const rigthImage = document.querySelector('.right-image img');
 
+            
+            var rImagePrice = document.querySelector('.price');
+            var rImageSale = document.querySelector('.offer');
+
+            //Loop que lerá cada top seller único
+
+            uniqueTopSellers.forEach(function (item) {
+                
+                if (item.name != "Steam Deck") {
+                    console.log(item.finalPrice);
+                    var finalPrice = (item.final_price /100).toFixed(2);
+                    //var originalPrice = (item.original_price / 100).toFixed(2);
+                    var sale = item.discount_percent
+
+                    
+                    //Não irei criar elementos HTML apenas dar setAtribute 
+                    rigthImage.setAttribute('src', item.large_capsule_image);
+                    rImagePrice.textContent =finalPrice;
+                    rImageSale.textContent = sale+"%"
+                }
+            });
     } else {
         console.log('Erro ao buscar os dados:', xhr.status);
     }
-};
-xhr.send();
+}else{
+    console.log("Estado atual do XMLHttpRequest:", xhr.readyState);
+}
+}
+xhr3.send();

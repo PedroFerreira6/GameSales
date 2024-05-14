171ae0c6a6178c5c3c9b3b5d63dfe0a99c46d055
Otaniel Junior <otaniel1412@gmail.com>
2024-05-14 12:29:26 +0100

    Login and register implementation

---
+++ login_register/js/login_register.js
@@ -0,0 +1,11 @@
+
+class uniqueTopSellers
+{
+    constructor( userName, email, password)
+    {
+        this.username = userName;
+        this.email = email;
+        this.password = password;
+    }
+}
+

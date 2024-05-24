b124d8c92ddab0103d9498a4ac17cb6ea9a41820
Otaniel Junior <otaniel1412@gmail.com>
2024-05-24 08:56:45 +0100

    login system implementation

--- login_register/js/login_register.js
+++ login_register/js/login_register.js
@@ -1,11 +1,25 @@
 
-class uniqueTopSellers
-{
-    constructor( userName, email, password)
-    {
+
+var signinButton = document.getElementById('signin');
+
+signinButton.addEventListener('click', function() {
+    var nameOfUser = document.getElementById('your_name');
+    var Htmlpassword = document.getElementById('your_pass');
+
+    var nameOfUserValue = nameOfUser.value;
+    var passwordValue = Htmlpassword.value;
+
+    console.log(nameOfUserValue);
+    console.log(passwordValue);
+});
+
+/* 
+class UniqueTopSellers {
+    constructor(userName, email, password) {
         this.username = userName;
         this.email = email;
         this.password = password;
     }
 }
+*/
 
